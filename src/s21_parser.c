a91b9060a08b9cd703ac6784d91b5cd22b47bf3f
Blangish <rebox3@gmail.com>
2023-05-21 14:28:41 +0300

    added matrix functions

--- src/s21_parser.c
+++ src/s21_parser.c
@@ -29,12 +29,51 @@ int parse_amount(data_t *data, char *path_to_file){
     return err;
 }
 
+int s21_create_matrix(unsigned int rows, unsigned int columns, matrix_t *result) {
+  int res = 0;
+  if (rows < 1 || columns < 1)
+    res = 1;
+  else {
+    result->rows = rows;
+    result->cols = columns;
+    result->matrix = (double **)calloc(rows, sizeof(double *));
+    if (result->matrix == NULL) {
+      res = 1;
+    } else {
+      for (unsigned int i = 0; i < rows; i++) {
+        result->matrix[i] = (double *)calloc(columns, sizeof(double));
+        if (result->matrix[i] == NULL) {
+          for (unsigned int j = 0; j < i; j++) {
+            free(result->matrix[j]);
+          }
+          free(result->matrix);
+          res = 1;
+          break;
+        }
+      }
+    }
+  }
+  return res;
+}
+
+void s21_print_matrix(matrix_t *matrix) {
+    for (unsigned int i = 0; i < matrix->rows; i++){
+        for (unsigned int j = 0; j < matrix->cols; j++) {
+            printf("%1.f ", matrix->matrix[i][j]);
+        }
+        printf("\n");
+    }
+}
+
 int main() {
     data_t *data = malloc(sizeof(data_t));
-    data->count_of_vertexes = *(unsigned int *)malloc(10 * sizeof(unsigned int));
-    data->count_of_facets = *(unsigned int *)malloc(10 * sizeof(unsigned int));
+    matrix_t matrix;
+    s21_create_matrix(3, 3, &matrix);
+    data->count_of_vertexes = *(unsigned int *)malloc(sizeof(unsigned int));
+    data->count_of_facets = *(unsigned int *)malloc(sizeof(unsigned int));
     char *path_to_file = "test.obj";
     parse_amount(data, path_to_file);
+    s21_print_matrix(&matrix);
     printf("amount of vertexes = [%d]\n", data->count_of_vertexes);
     printf("amount of facets = [%d]\n", data->count_of_facets);
 }
--- src/s21_parser.h
+++ src/s21_parser.h
@@ -19,12 +19,14 @@ typedef struct facets {
 typedef struct data {
     unsigned int count_of_vertexes;  //Кол-во вершин
     unsigned int count_of_facets;    //Кол-во полигонов
-    matrix_t martix_3d;
+    matrix_t martix_3d; 
     polygon_t *polygons;
 }data_t;
 
 
-
+int parse_amount(data_t *data, char *path_to_file);
+int s21_create_matrix(unsigned int rows, unsigned int columns, matrix_t *result);
+void s21_print_matrix(matrix_t *matrix);
 
 
 #endif //S21_PARSER_H_
